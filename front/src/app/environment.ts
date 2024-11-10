b21bfc59725cab075fd7b0398e27f744292a441f
Ibrahim Krimi <ibrahimkrimi2@gmail.com>
2024-11-11 00:41:43 +0100

    modify env angular

--- front/src/app/environment.ts
+++ front/src/app/environment.ts
@@ -1,6 +1,13 @@
 export const environment = {
   production: false,
-  userserviceAPI: 'http://localhost:8080',
-  productserviceAPI: 'http://localhost:8081',
-  orderserviceAPI: 'http://localhost:8082',
+  userserviceAPI: "http://userservice.default.svc.cluster.local:8080"
+  //'http://localhost:8080'
+  ,
+  productserviceAPI:"http://productservice.default.svc.cluster.local:8081"
+  //'http://localhost:8081'
+  ,
+  orderserviceAPI: "http://orderservice.default.svc.cluster.local:8082/orderservice"
+  ,
+  //'http://localhost:8082'
+
 };
